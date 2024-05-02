a431141b4128997c19873153bef8a4eab272a001
galina <galina.ramodina@gmail.com>
2024-05-02 12:40:33 +0300

    created project

---
+++ src/main/java/hogwarts/schoolsql/SchoolSqlApplication.java
@@ -0,0 +1,13 @@
+package hogwarts.schoolsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSqlApplication.class, args);
+	}
+
+}
